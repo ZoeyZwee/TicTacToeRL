5b45f939a320f6bb0a32debdfe2f12bc173d3625
ZoeyZwee <thisemailistaken23@gmail.com>
2022-12-27 14:55:37 -0500

    Minor changes and refactoring

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from TicTacToe import Board, GameStatus
-from TicTacAgent import MonteCarloAgent
+from MonteCarloAgent import MonteCarloAgent, play_match
 import random
-
-from src.MCPlotter import MCPlotter
+import json
+from MCPlotter import MCPlotter
+import numpy as np
 
 
 def human_v_human():
@@ -45,20 +46,22 @@ def monte_v_monte():
     :return:
     """
 
+    # learning params
+    epsilon = 0.01  # % chance of a random move. updated periodically after playing some number of games
+    gamma = 0.90  # decay rate for rewards
+    alpha = 0.02  # learning rate
     games_played = 0
-    epsilon = 1  # % chance of a random move. epsilon = 1/(k+1) where k is games played
 
-    board = Board()
-    p1 = MonteCarloAgent(1, board)
-    p2 = MonteCarloAgent(2, board)
+
+    p1 = MonteCarloAgent(1)
+    p2 = MonteCarloAgent(2)
     mcplot = MCPlotter(p1, p2)
 
     startermove = 1
 
     plot = False  # only plot if we've seen all the start states (plotter code breaks otherwise)
-    while True:
-        game_log = []  # list of game states from THIS game
-        # play a game
+    while games_played < 1_000_000:
+
         # start with each opener evenly
         match startermove:
             case 1:
@@ -67,65 +70,69 @@ def monte_v_monte():
                 startermove = 5
             case 5:
                 startermove = 1
-                plot = True  # all start states seem from here on out
-        p1.play_move(startermove)
-        game_log.append(board.copy())
-        epsilon_move(p2, epsilon)
-        game_log.append(board.copy())
-        while True:
-            # p1 plays
-            status = epsilon_move(p1, epsilon)
-            game_log.append(board.copy())
-            if status != GameStatus.RUNNING:
-                break
-
-            # p2 plays
-            status = epsilon_move(p2, epsilon)
-            game_log.append(board.copy())
-            if status != GameStatus.RUNNING:
-                break
-
+                plot = True  # all start states seen - now safe to plot
+        # play match
+        outcome, game_log = play_match(p1, p2, epsilon, startermove)
+        games_played += 1
 
         # update value fns
-        p1.update_value(game_log, status, gamma=GAMMA, alpha=ALPHA)
-        p2.update_value(game_log, status, gamma=GAMMA, alpha=ALPHA)
+        p1.update_value(game_log, REWARDS[outcome], gamma=gamma, alpha=alpha)
+        p2.update_value(game_log, REWARDS[outcome], gamma=gamma, alpha=alpha)
 
-        # reset board
-        board = Board()
-        p1.new_game(board)
-        p2.new_game(board)
-
-        # logging
+        # logging and plotting
         if plot:
-            mcplot.log_and_plot(status)
+            openers = {1: "Corner", 2: "Side", 5: "Centre"}
+            mcplot.log_and_plot(outcome, opener=openers[startermove])
 
-        games_played += 1
-        epsilon = 1/(games_played+1)
-        # print(status)
-        # print("D  P1  P2")
-        # print(win_log, f"P1 wr: {win_log[1]/sum(win_log):.3%}, P2 wr: {win_log[2]/sum(win_log):.3%}")
+        # save value fn
+        if games_played % 500 == 0:
+            with open(f"MCValue.json", "w") as f:
+                print(f"saving value function... ({len(p1.value)} states seen, {games_played} games played)")
+                d = {str(board): val for board, val in p1.value.items()}
+                json.dump(d, f)
 
 
-def epsilon_move(agent, epsilon):
-    """
-    get agent to play a move with epsilon chance of playing a random move
-    :param agent: agent which is playing the move
-    :param epsilon: chance we play a random move
-    :return: RUNNING_STATE of game after executing move
-    """
+def inspect_value():
+    value_path = "D:\Programming\GithubRepos\TicTacToeRL\src\MCValue.json"
+    optimal_value_path = "D:\Programming\GithubRepos\TicTacToeRL\src\DPSolver.py"
+    board = Board()
+    p1 = MonteCarloAgent(1, board)
+    p2 = MonteCarloAgent(2, board)
 
-    if random.random() < epsilon:
-        return agent.play_random()
+    p1.load_value(value_path)
+    p2.load_value(value_path)
+
+    for startermove in [1, 2, 5]:
+        outcome, gamelog = play_match(p1, p2, epsilon=0, startermove=startermove)
+        for boardstate in gamelog:
+            print(f"value={p1.value[boardstate]}")
+            print(boardstate)
+            print("\n" * 3)
+
+    opt_agent = MonteCarloAgent(1, board)
+    try:
+        opt_agent.load_value(optimal_value_path)
+    except FileNotFoundError:
+        print("Couldn't load optimal value function")
     else:
-        return agent.play_move(agent.get_best_move())
+        non_draws_missing = 0  # number of "winning" states not found in p1.value
+        wins_missing = 0
+        sq_error = 0
+        for state in opt_agent.value:
+            if state in p1.value:
+                sq_error += (p1.value[state]-opt_agent.value[state])**2
+            else:
+                non_draws_missing += (opt_agent.value[state] != 0)
+                wins_missing += (opt_agent.value[state] == 1) or (opt_agent.value[state] == -1)
 
+        rmse = np.sqrt((sq_error/len(p1.value)))
+        print(f"Among seen states, value function has average error (RMSE) of {rmse}")
+        print(f"Among {len(opt_agent.value)-len(p1.value)} unseen states, {non_draws_missing} do not result in a draw")
 
 
 
 if __name__ == "__main__":
-    # PARAMETERS
-    GAMMA = 0.90
-    ALPHA = 0.01
+
     REWARDS = {
         GameStatus.DRAW: 0,
         GameStatus.P1_WIN: 1,
@@ -136,11 +143,10 @@ if __name__ == "__main__":
     print("Please select an option...")
     print("1: Human v Human")
     print("2: MonteCarlo v MonteCarlo")
-
-    options = {1: human_v_human, 2: monte_v_monte}
+    print("3: Inspect/test value function")
+    options = {1: human_v_human, 2: monte_v_monte, 3: inspect_value}
     str_in = input()
     success = False
-    options[int(str_in)]()
     while success is False:
         try:
             choice = options[int(str_in)]
@@ -149,4 +155,3 @@ if __name__ == "__main__":
             print("Invalid input:", e)
             str_in = input()
     choice()
-    
